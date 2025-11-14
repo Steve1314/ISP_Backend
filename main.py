3e313ebe1ff96c2470d974681fb959e66ffb64f6
Vivek Prajapati <Vprajapati@vahlay.local>
2025-11-14 22:16:58 +0530

    Connected to MongoDB Atlas

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
-import os
 
 app = FastAPI(title="ISP Finder API")
 
+# ---------------- CORS ----------------
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,21 +12,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MONGO_URI = os.getenv("MONGO_URI")
-if not MONGO_URI:
-    raise Exception("❌ Missing MONGO_URI")
+# ---------------- MongoDB Atlas ----------------
+MONGO_URI = "mongodb+srv://vivek:vivek@cluster0.avwiuf8.mongodb.net/isp_db?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
 db = client["isp_db"]
 collection = db["providers"]
 
+# ---------------- Routes ----------------
+
 @app.get("/")
 def home():
-    return {"status": "running"}
+    return {"status": "API Running"}
 
 @app.get("/providers/{zip_code}")
 def get_providers(zip_code: str):
     providers = list(collection.find({"zip": zip_code}, {"_id": 0}))
+    
     if not providers:
         raise HTTPException(status_code=404, detail="No providers found for this ZIP")
+    
     return {"zip": zip_code, "providers": providers}
